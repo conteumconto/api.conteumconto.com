d85c380992f5f0ee6f627b3dbfdbf67403331fcc
Luan Andryl <luan.andryl@gmail.com>
2017-04-04 06:41:51 -0300

    app bootstrap

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
+/*
+  Common
+*/
 import express from 'express'
-import area from './routes/area'
+import path from 'path'
+import logger from 'morgan'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
+/*
+  Database Import
+*/
+import Database from './database/Database'
+/*
+  Endpoints
+*/
+import studentRouter from './routes/Student'
+
+
 let app = express()
 
-app.use('/', area)
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+/*
+  [Database conection] -> refactor
+*/
+const conn = new Database('local')
+
+/*
+  routes to student resource
+*/
+app.use('/student', studentRouter)
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error')
+})
 
 app.listen(3000, () => {
 	console.log('Listening on 3000')
