4df3ba0af63218fafda2b515c8651bfab72fb68e
Luan Andryl <luan.andryl@gmail.com>
2017-04-04 12:53:06 -0300

    create chapter schema

---
+++ src/models/schema/Chapter.js
@@ -0,0 +1,29 @@
+'use strict'
+import mongoose from 'mongoose'
+/**
+ * Restrictions
+ */
+const bookRestriction = [{
+  type: mongoose.Schema.Types.ObjectId,
+  ref: 'Book',
+  required: true,
+}];
+
+const titleRestriction = {
+  type: String,
+  required: [true, 'no title given'],
+  minlength: [1, 'title is too short'],
+  max: [40, 'title is too long'],
+};
+
+const chapterTextRestriction = {
+    type: String
+}
+
+const chapterSchema = new mongoose.Schema({
+  _book: bookRestriction,
+  title: titleRestriction,
+  chapterText: chapterTextRestriction,
+});
+
+export default mongoose.model('Chapters', chapterSchema);
