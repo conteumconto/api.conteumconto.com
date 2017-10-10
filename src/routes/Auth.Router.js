b70e49fdf90721765606c40e13233dc49337bc7c
luandryl <luan.andryl@gmail.com>
2017-10-10 19:12:04 -0300

    auth routes

---
+++ src/routes/Auth.Router.js
@@ -0,0 +1,28 @@
+import express from 'express'
+/*
+  Import the resource controller, the code below its pretty intuitive :3
+*/
+import AuthController from '../controllers/Auth.Controller'
+
+let router = express.Router()
+/*
+  import student RESOURCE CONTROLLER 
+*/
+let auth = new AuthController()
+
+
+/*
+  routing the controller object through student resource endpoints
+*/
+
+router.post('/login', (req, res) => {
+  auth.login(req, res)
+})
+
+router.post('/singup', (req, res) => {
+  auth.singup(req, res)
+})
+
+
+
+export default router
