ff68e720c7f8108bfb86752ba88fa7432ff3860d
luandryl <luan.andryl@gmail.com>
2017-10-10 19:11:55 -0300

    user scheme

---
+++ src/models/schema/User.js
@@ -0,0 +1,44 @@
+'use strict'
+import mongoose from 'mongoose'
+// This model refers to the creation of any person(Student, Teacher...), which will be inherited by other models.
+
+/**
+ * Restrictions
+ */
+
+const nameRestriction = {
+  type: String,
+  required: [true, 'No name given'],
+  minlength: [3, 'Name too short'],
+  maxlength: [100, 'Name too big'],
+};
+
+const birthDayRestriction = {
+  type: String,
+  required: [true, 'No birth day given'],
+};
+
+const loginRestriction = {
+  type: String,
+  required: [true, 'No login given'],
+  index: {
+      unique: true,
+  },
+};
+
+const passwordRestriction = {
+  type: String,
+  required: [true, 'No password given'],
+};
+
+
+// Create Schema
+const userSchema = new mongoose.Schema({
+  first_name: nameRestriction,
+  last_name: nameRestriction,
+  birth_day: birthDayRestriction,
+  login: loginRestriction,
+  password: passwordRestriction,
+});
+
+export default mongoose.model('User', userSchema);
