ca7d0285d45e5836fd2a9cf1ceed615d055bd526
luandryl <luan.andryl@gmail.com>
2017-10-11 17:00:11 -0300

    classes controller

---
+++ src/controllers/Class.Controller.js
@@ -0,0 +1,31 @@
+'use strict';
+import BaseController from './Base.Controller'
+import Class from '../models/Class.Model'
+
+export default class ClassController extends BaseController {
+
+  constructor() {
+    super(Class)
+  }
+
+  teacherClasses (req, res) {
+    let data = {
+      teacher: req.params.teacher_id
+    }
+
+    let classModel = new Class(data).getByField()
+
+    Promise.all([
+      classModel
+    ]).then((classes) => {
+      if (classes) {
+        res.json(classes[0])
+        res.status(200)
+      }
+    }).catch(err => {
+      console.log(err)
+    })
+
+  }
+
+}
