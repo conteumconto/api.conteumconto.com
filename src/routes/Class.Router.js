f01deda64242a5bcee6e8964ff98e18c095d6dff
luandryl <luan.andryl@gmail.com>
2017-10-11 17:00:45 -0300

    classes routes

---
+++ src/routes/Class.Router.js
@@ -0,0 +1,47 @@
+import express from 'express'
+import passport from 'passport' 
+
+import Class from '../controllers/Class.Controller'
+
+
+const protect = passport.authenticate('jwt', {
+  session: false,
+});
+
+let router = express.Router()
+/*
+  import student RESOURCE CONTROLLER 
+*/
+let cl = new Class()
+
+router.get('/', protect, (req, res) => {
+    res.json({
+      'msg': 'Welcome to class endpoints' 
+    })
+}) 
+
+router.post('/', protect, (req, res) => {
+  cl.save(req, res)
+})
+
+router.get('/:id', protect, (req, res) => {
+  cl.getById(req, res)
+})
+ 
+router.put('/:id', protect, (req, res) => {
+  cl.updateById(req, res)
+})
+
+router.delete('/:id', protect, (req, res) => {
+  cl.removeById(req, res)
+})
+
+
+
+router.get('/teacher/:teacher_id', protect, (req, res) => {
+  cl.teacherClasses(req, res)
+})
+
+
+export default router
+
