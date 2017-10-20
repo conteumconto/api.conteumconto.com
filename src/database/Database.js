d40e2be1befda225eb9cc6db0bbc20a12c5d7be2
luandryl <luan.andryl@gmail.com>
2017-10-20 01:21:18 -0200

    database conection fixe

--- src/database/Database.js
+++ src/database/Database.js
@@ -3,29 +3,26 @@ import mongoose from 'mongoose'
 import config from '../config/database'
 
 export default class Database {
-	static init () {
+	init () {
 		mongoose.Promise = global.Promise
 		return process.env.DEV ? this._local() : this._production()
 	}
 
-	static _production () {
-		console.log('mongodb://heroku_z1rp5mnh:9idkpr8q4q5ef78nifns2p1259@ds117615.mlab.com:17615/heroku_z1rp5mnh')
-		return mongoose.createConnection('mongodb://heroku_z1rp5mnh:9idkpr8q4q5ef78nifns2p1259@ds117615.mlab.com:17615/heroku_z1rp5mnh', {
-			useMongoClient: true
-		}).then(() => {
+	_production () {
+		return mongoose.connect(process.env.MONGO_URI).then(() => {
 			console.log('Database connected successfully')
+			return true
 		}).catch((err) => {
 			console.error(err)
 		})
 	}
 
-	static _local () {
+	_local () {
 		const localURI = 'mongodb://' + config.dev.local.host + ':' +
 											config.dev.local.port + '/' + config.dev.local.database
 
-		return mongoose.createConnection(localURI, {
-			useMongoClient: true
-		}).then(() => {
+		return mongoose.connect(localURI).then(() => {
+			console.log('Database connected successfully')
 			return true
 		}).catch((err) => {
 			console.error(err)
