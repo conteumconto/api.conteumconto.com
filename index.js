4703fab5b25a4160bbd51ae312e9572aa0c4c487
luandryl <luan.andryl@gmail.com>
2017-10-23 05:46:58 -0200

    build test v1.15

--- index.js
+++ index.js
@@ -1,50 +1,74 @@
-var express = require('express');
-var path = require('path');
-var serverStatic = require('serve-static');
-var app = express();
+/*
+	Common
+*/
+import express from 'express'
+import path from 'path'
+import logger from 'morgan'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
+import passport from 'passport'
+/*
+	Database Import
+*/
+import Database from './src/database/Database'
+/*
+	Endpoints
+*/
+import student from './src/routes/Student.Router'
+import book from './src/routes/Book.Router'
+import chapter from './src/routes/Chapter.Router'
+import cls from './src/routes/Class.Router'
+import auth from './src/routes/Auth.Router'
+/*
+	middleware
+*/
+import protectMiddleware from './src/middleware/passport'
+protectMiddleware(passport)
 
-app.use(serverStatic(__dirname));
+let app = express()
 
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, res) {
-	res.json({
-		'Msg': 'welcome'
-	})
+/*
+	[Database conection]
+*/
+const conn = new Database()
+conn.init()
+
+/*
+	routes to student resource
+*/
+app.use('/student', student)
+app.use('/book', book)
+app.use('/chapter', chapter)
+app.use('/class', cls)
+app.use('/auth', auth)
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+	const err = new Error('Not Found')
+	err.status = 404
+	next(err)
 })
 
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// error handler
+app.use((err, req, res) => {
+	// set locals, only providing error in development
+	res.locals.message = err.message
+	res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-/// error handlers
+	// render the error page
+	res.status(err.status || 500)
+	res.render('error')
+})
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+var port = process.env.PORT || 5000
+app.listen(port, function () {
+	console.log("Running on port: ", port)
+})
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
+export default app
