17eca60f8d02e664d9618f6072e2783327b97ae2
Davi Kawasaki <davishinjik@gmail.com>
2017-11-03 13:17:33 -0200

    Adding CORS to routes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import logger from 'morgan'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 import passport from 'passport'
+import cors from 'cors'
 /*
 	Database Import
 */
@@ -24,11 +25,13 @@ import teacher from './src/routes/Teacher.Router'
 	middleware
 */
 import PassportMiddleware from './src/middleware/Passport.Middleware'
-const protect = new PassportMiddleware(passport).protect()
+const protect = new PassportMiddleware(passport)
+protect.protect()
 
 let app = express()
 
 app.use(logger('dev'))
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
@@ -68,10 +71,10 @@ app.use((err, req, res) => {
 	res.render('error')
 })
 
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 3000
 
 app.listen(port, function () {
-	console.log("Running on port: ", port)
+	console.log('Running on port: ', port)
 })
 
 export default app
