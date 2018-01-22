8ac4d23f844e2c21eb3501b633ffb2abb5c5e68b
Davi Kawasaki <davishinjik@gmail.com>
2018-01-22 00:20:29 -0200

    Changing user birth restriction to age

--- src/models/schemes/User.js
+++ src/models/schemes/User.js
@@ -7,38 +7,37 @@ import mongoose from 'mongoose'
  */
 
 const nameRestriction = {
-  type: String,
-  required: [true, 'No name given'],
-  minlength: [3, 'Name too short'],
-  maxlength: [100, 'Name too big'],
-};
+	type: String,
+	required: [true, 'No name given'],
+	minlength: [3, 'Name too short'],
+	maxlength: [100, 'Name too big']
+}
 
-const birthDayRestriction = {
-  type: String,
-  required: [true, 'No birth day given'],
-};
+const ageRestriction = {
+	type: String,
+	required: [true, 'No age given']
+}
 
 const loginRestriction = {
-  type: String,
-  required: [true, 'No login given'],
-  index: {
-      unique: true,
-  },
-};
+	type: String,
+	required: [true, 'No login given'],
+	index: {
+		unique: true
+	}
+}
 
 const passwordRestriction = {
-  type: String,
-  required: [true, 'No password given'],
-};
-
+	type: String,
+	required: [true, 'No password given']
+}
 
 // Create Schema
 const userSchema = new mongoose.Schema({
-  first_name: nameRestriction,
-  last_name: nameRestriction,
-  birth_day: birthDayRestriction,
-  login: loginRestriction,
-  password: passwordRestriction,
-});
+	first_name: nameRestriction,
+	last_name: nameRestriction,
+	age: ageRestriction,
+	login: loginRestriction,
+	password: passwordRestriction
+})
 
 export default mongoose.model('User', userSchema);
