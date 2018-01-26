ed1f97cdcf3daac7c7cf5b487308542035e7f36f
Davi Kawasaki <davishinjik@gmail.com>
2018-01-26 18:14:50 -0200

    AddingChapterToList method for Book Model

--- src/models/Book.Model.js
+++ src/models/Book.Model.js
@@ -23,4 +23,19 @@ export default class BookModel extends BaseModel {
 	constructor (data) {
 		super(Book, '_id', data)
 	}
+
+	/**
+	 * Book AddingChapterToList Method.
+	 * Query this.Schema pushing new chapter to list.
+	 * When this promise is resolved this method retrieves the Object update in MongoDB.
+	 *
+	 * @name AddingChapterToList
+	 * @param {String} bookId
+	 * @param {Object} chapterId - Chapter Object ID
+	 * @method addingChapterToList
+	 * @return {object} Return a promise to who intend to update data.
+	*/
+	AddingChapterToList (bookId, chapterId) {
+		return this.Scheme.update({_id: bookId}, {$push: {chapters: chapterId}}, { new: true })
+	}
 }
