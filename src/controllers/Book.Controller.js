ba0f45e93e571a282cc17c64efc51414b7613d6a
Davi Kawasaki <davishinjik@gmail.com>
2018-01-26 20:37:45 -0200

    SaveBook with promise chain (saving it in user books array too) + removeBook + getStudentsBooks by list of IDs + removeChapterFromList

--- src/controllers/Book.Controller.js
+++ src/controllers/Book.Controller.js
@@ -12,6 +12,8 @@
  */
 'use strict'
 import BaseController from './Base.Controller'
+import StudentController from './Student.Controller'
+import ChapterController from './Chapter.Controller'
 import Book from '../models/Book.Model'
 import Student from '../models/Student.Model'
 
@@ -28,45 +30,101 @@ export default class BookController extends BaseController {
 	 * If error return 400 status code and a json => { errors }.
 	 * 500 status code only will be returned if the method generates some unexpected error.
 	 *
-	 * @name Save
+	 * @name SaveBook
 	 * @param {object} req - Express requisition object.
 	 * @param {object} res - Express response object.
 	 * @return {json} status and return object.
-	 * @method save
-	 * @todo Write comments
+	 * @method saveBook
 	*/
 	saveBook (req, res) {
 		let bookWithStudent = req.body
 		bookWithStudent._student = req.user._id
 
-		let bookPromise = new Promise((resolve, reject) => {
-			new this.Model(bookWithStudent).persist()
-				.then((data) => {
-					this._book = data
-					return resolve(this._book._id)
-				})
-				.catch((err) => {
-					reject(err)
-				})
-		})
+		// 1. Save book in DB, returning its ID
+		new this.Model(bookWithStudent).persist()
+			.then(data => {
+				this._book = data
+				return this._book._id
+			})
+			// 2. Add book ID to student books array list
+			.then(id => new Student().AddingBookToList(req.user._id, id))
+			// 3. Return book
+			.then(data => {
+				if (data) res.status(200).json(this._book)
+				else throw new Error('book_not_saved')
+			})
+			.catch(err => {
+				console.error(err)
+				if (err.message === 'book_not_saved') res.status(400).json(err.message).end()
+				else res.status(500).json(err.message).end()
+			})
+	}
 
-		let userPromise = bookPromise.then((id) => {
-			return new Student().AddingBookToList(req.user._id, id)
-		})
+	/**
+	 * RemoveBook Method.
+	 * Responds to DELETE /book/:id.
+	 * Delete book by its ID and remove itself from user book list.
+	 * If Success returns 200 status code and a json with the deleted book.
+	 * If error return 400 status code and a json => { errors }.
+	 * 500 status code only will be returned if the method generates some unexpected error.
+	 *
+	 * @name RemoveBook
+	 * @param {object} req - Express requisition object.
+	 * @param {object} res - Express response object.
+	 * @return {json} status and return object.
+	 * @method RemoveBook
+	 * @todo Write comments
+	 */
+	removeBook (req, res) {
+		let data = {
+			_id: req.params.id
+		}
 
-		Promise.all([
-			bookPromise, userPromise
-		]).then((data) => {
-			if (data) {
-				res.send(this._book)
-				res.status(200)
-				res.end()
-			}
-		}).catch(err => {
-			console.log(err)
-			res.json(err)
-			res.status(400)
-			res.end()
+		let bookPromise = new this.Model(data).deleteById()
+		let chapterPromise = new ChapterController().removeByBookIdList([].concat(req.params.id))
+		let userPromise = new StudentController().removeBookFromList(req.user._id, req.params.id)
+
+		Promise.all([bookPromise, chapterPromise, userPromise])
+			.then(response => {
+				if (!response[0]) throw new Error('object_not_deleted')
+				else if (!response[1]) throw new Error('object_not_deleted')
+				else if (!response[2]) throw new Error('object_not_updated')
+				else res.status(200).json(response[0]).end()
+			})
+			.catch(err => {
+				console.error(err)
+				if (err.message === 'object_not_deleted' || err.message === 'object_not_updated') {
+					res.status(400).json(err.message).end()
+				} else res.status(500).json(err.message).end()
+			})
+	}
+
+	/**
+	 * Get student books from books ID list
+	 * @name GetStudentBooks
+	 * @param {array} books
+	 * @return {json} status and return array list of full books.
+	 * @method getStudentBooks
+	 */
+	getStudentBooks (books) {
+		return new this.Model().findByIdList({
+			'_id': { $in: books }
 		})
+	}
+
+	/**
+	 * Get book chapters and filter through passed chapter ID
+	 * @name RemoveChapterFromList
+	 * @param {Long} bookId
+	 * @param {Long} chapterId
+	 * @method removeChapterFromList
+	 */
+	removeChapterFromList (bookId, chapterId) {
+		return new this.Model({_id: bookId}).getById()
+			.then(book => {
+				book = book[0]
+				book.chapters = book.chapters.filter(chapter => chapter.toString() !== chapterId)
+				return new this.Model(book).updateById()
+			})
 	}
 }
